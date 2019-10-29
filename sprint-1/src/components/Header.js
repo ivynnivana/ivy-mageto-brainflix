3e103e491cd4593e9752370d50577c1730c526db
Ivy Mageto <ivymageto@MirLeos-iPhone.localdomain>
2019-10-29 13:53:53 -0700

    added new content to the main section

--- sprint-1/src/components/Header.js
+++ sprint-1/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Logo from "../assets/Logo/Logo-brainflix.svg"
-// import Icons from "../assets/Icons/SVG/Icon-search.svg"
 import Images from "../assets/Images/Mohan-muruge.jpg"
 
 export default function Header() {
@@ -12,11 +11,10 @@ export default function Header() {
                 </div>
                 <div>
                 <input className="header-input" type="text" name="name" value="" placeholder= "Search"  />
-                </div>
-                <div>
-                    <button>+ UPLOAD</button>
+                    <button className="header-button">UPLOAD</button>
                     <img className="header-image" src={Images} alt="Mohan image"/>
                 </div>
+               
             </div>
             
         </header>
