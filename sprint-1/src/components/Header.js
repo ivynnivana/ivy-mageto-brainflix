985003289152f83d0e9d74bedbdc876ca79a2f26
Ivy Mageto <ivymageto@Ivys-MacBook-Pro.local>
2019-10-28 22:47:25 -0700

    added more info to maincontent

--- sprint-1/src/components/Header.js
+++ sprint-1/src/components/Header.js
@@ -11,13 +11,14 @@ export default function Header() {
                 <img className="header-logo" src={Logo} alt=""/>
                 </div>
                 <div>
-                <input className="header-input" type="text" name="name" value="" placeholder= "{Icons} Search"  />
+                <input className="header-input" type="text" name="name" value="" placeholder= "Search"  />
                 </div>
                 <div>
                     <button>+ UPLOAD</button>
                     <img className="header-image" src={Images} alt="Mohan image"/>
                 </div>
             </div>
+            
         </header>
     )
 }
--- sprint-1/src/components/MainContent.js
+++ sprint-1/src/components/MainContent.js
@@ -7,6 +7,10 @@ export default function MainContent(){
             <video className="main-video" controls>
                 <source src={Video} type="video/mp4"></source>
             </video>
+            <div><h1 className="main-title">BMX Rampage: 2018 Highlights</h1></div>
+            <div className="main-author"><p>By Red Crow</p>
+            <p >12/18/2018</p></div>
+            <div><p>On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title</p></div>
         </main>
     )
 }
