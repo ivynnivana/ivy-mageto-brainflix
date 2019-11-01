deddff31c1224b3f076adf08e2b12a55d415c455
Ivy Mageto <ivymageto@MirLeos-iPhone.localdomain>
2019-11-01 16:48:05 -0700

    made changes

--- sprint-1/src/components/MainContent.js
+++ sprint-1/src/components/MainContent.js
@@ -1,27 +1,30 @@
 import React from "react";
-// import Video from "../assets/Video/BrainStation Sample Video.mp4";
+import Vid from "../assets/Images/brainstation.mp4";
 import Images from "../assets/Images/video-list-0.jpg";
-import PlayButton from "../assets/Icons/SVG/Icon-play.svg";
-import FullScreenButton from "../assets/Icons/SVG/Icon-fullscreen.svg";
-import VolumeButton from "../assets/Icons/SVG/Icon-volume.svg";
-// import LikesButton from "../assets/Icons/SVG/Icon-likes.svg";
-// import ViewsButton from "../assets/Icons/SVG/Icon-views.svg";
-// import MohanImg from "../assets/Images/Mohan-muruge.jpg";
+// import PlayButton from "../assets/Icons/SVG/Icon-play.svg";
+// import FullScreenButton from "../assets/Icons/SVG/Icon-fullscreen.svg";
+// import VolumeButton from "../assets/Icons/SVG/Icon-volume.svg";
 
 export default function MainContent() {
   return (
     <main>
       <div>
         <div className="img-div">
-          <img className="main-image" src={Images} alt="" />
+          <video
+            className="main-image"
+            src={Vid}
+            controls
+            poster={Images}
+          ></video>
+          {/* <img className="main-image" src={Images} alt="" /> */}
         </div>
-        <div className="sub-img">
+        {/* <div className="sub-img">
           <img className="sub-img2" src={PlayButton} alt="" />
           <div>
             <img className="sub-img3" src={FullScreenButton} alt="" />
             <img className="sub-img3" src={VolumeButton} alt="" />{" "}
           </div>
-        </div>
+        </div> */}
       </div>
     </main>
   );
